9d654774e3a3c25097ade86e13a97e7f92017e1b
Loc <tanlocit9@gmail.com>
2021-02-09 02:54:45 +0700

    Backend - Modify file modal.js

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,9 +1,9 @@
 var openmodal = document.querySelectorAll('.modal-open')
     for (var i = 0; i < openmodal.length; i++) {
-      openmodal[i].addEventListener('click', function(event){
-    	event.preventDefault()
-    	toggleModal()
-      })
+        openmodal[i].addEventListener('click', function(event){
+            event.preventDefault()
+            toggleModal()
+        })
     }
 
     const overlay = document.querySelector('.modal-overlay')
@@ -11,20 +11,20 @@ var openmodal = document.querySelectorAll('.modal-open')
 
     var closemodal = document.querySelectorAll('#modal-close')
     for (var i = 0; i < closemodal.length; i++) {
-      closemodal[i].addEventListener('click', toggleModal)
+        closemodal[i].addEventListener('click', toggleModal)
     }
 
     document.onkeydown = function(evt) {
-      evt = evt || window.event
-      var isEscape = false
-      if ("key" in evt) {
-    	isEscape = (evt.key === "Escape" || evt.key === "Esc")
-      } else {
-    	isEscape = (evt.keyCode === 27)
-      }
-      if (isEscape && document.body.classList.contains('modal-active')) {
-    	toggleModal()
-      }
+        evt = evt || window.event
+        var isEscape = false
+        if ("key" in evt) {
+            isEscape = (evt.key === "Escape" || evt.key === "Esc")
+        } else {
+            isEscape = (evt.keyCode === 27)
+        }
+        if (isEscape && document.body.classList.contains('modal-active')) {
+            toggleModal()
+        }
     };
 
 
